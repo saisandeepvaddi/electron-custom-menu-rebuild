8e92ccc44cc1868702558c3cf9129b3ed931c215
Sai Sandeep Vaddi <saisandeepvaddi@gmail.com>
2020-02-01 22:31:15 -0500

    update readme

--- menu.js
+++ menu.js
@@ -6,34 +6,6 @@ const template = [
   {
     label: "File",
     submenu: [isMac ? { role: "close" } : { role: "quit" }]
-  },
-  // { role: 'editMenu' }
-  {
-    label: "Edit",
-    submenu: [
-      { role: "undo" },
-      { role: "redo" },
-    ]
-  },
-  // { role: 'viewMenu' }
-  {
-    label: "View",
-    submenu: [
-      { role: "reload" },
-      { role: "toggledevtools" },
-    ]
-  },
-  {
-    role: "help",
-    submenu: [
-      {
-        label: "Learn More",
-        click: async () => {
-          const { shell } = require("electron");
-          await shell.openExternal("https://github.com/saisandeepvaddi");
-        }
-      }
-    ]
   }
 ];
 
